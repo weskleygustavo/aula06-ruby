be19bb8c6673c9934550cc4976c76992543fc462
weskleygustavo <72476446+weskleygustavo@users.noreply.github.com>
2020-10-13 14:30:00 -0300

    Add files via upload

---
+++ Exemplo01.rb
@@ -0,0 +1,20 @@
+class Bola
+
+  attr_accessor :cor, :circunferencia1, :material
+
+  def initialize(cor, circunferencia1, material)
+    @cor = cor
+    @circunferencia1 = circunferencia1
+    @material = material
+
+  end
+
+  bola1 = Bola.new("Vermelho", "90", "ferro")
+  bola1.cor = "Amarelo"
+
+  puts bola1.cor
+  puts bola1.circunferencia1
+  puts bola1.material
+
+  
+  end
